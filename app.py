f00a6b920db41193ec18c5c54d5a10199c0c98d6
Tixu64 <murielraynaud@free.fr>
2021-01-30 08:05:26 +0100

    test2

--- app.py
+++ app.py
@@ -10,12 +10,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-import shap
-import _io as io
-import base64
-#from shap.plots._force_matplotlib import draw_additive_plot
-from sklearn.metrics import confusion_matrix
-import pickle
+
 
 
 
