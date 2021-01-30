194a246d6f3906efc6044fc0f47eafc4b3588ace
Tixu64 <murielraynaud@free.fr>
2021-01-30 14:21:01 +0100

    test2

--- app.py
+++ app.py
@@ -17,6 +17,14 @@ import pickle
 from shap import force_plot
 
 
+with open('df_client_dash.pkl','rb') as fp:
+    df_client= pickle.load(fp)
+
+
+
+with open('encoded_logo.pkl','rb') as fp:
+    encoded_logo= pickle.load(fp)   
+
 external_stylesheets = ['https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -26,11 +34,14 @@ server = app.server
 app.layout = html.Div(style={'backgroundColor': "color"},children=[
     
 
-
+                    html.Img(src='data:image/png;base64,{}'.format(encoded_logo)),
                     html.H1(children='Tableau de bord "Prêt à depenser" '),
                     html.Label(['Choisissez un client:'],style={'font-weight': 'bold', "text-align": "left"}),
     
-                   
+                    dcc.Dropdown(id='dropdown_client', options=[
+                    {'label': i, 'value': i} for i in df_client.SK_ID_CURR.sort_values().unique()
+                        ], multi=False, placeholder='Please select...',style=dict(width='50%')),
+                    
 
 ])
 
